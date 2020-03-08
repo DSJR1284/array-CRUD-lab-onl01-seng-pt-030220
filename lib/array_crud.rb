5136058a742a6259f9c8023c2fc9d4f59734729e
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-08 14:17:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,6 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+   sports_array =["Knicks", "Yankess","Nets", "Giants"]
+   sports_array
   
 end
 
