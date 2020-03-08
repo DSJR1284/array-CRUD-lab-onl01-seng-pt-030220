e0a01d77c4fbbe95f3bc83a55f0cf2b9567dedc4
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-08 14:19:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+   sports_array =["Knicks", "Yankess","Nets", "Giants"]
+   sports_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
