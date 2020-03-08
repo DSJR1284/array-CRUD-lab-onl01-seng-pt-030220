a572ae5012aea84bdb6f2d1adf37324ef4801760
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-08 14:24:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,35 +6,37 @@ def create_an_array
   sports_array =["Knicks", "Yankess","Nets", "Giants"]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(sports_array, element)
   sports_array =["Knicks", "Yankess","Nets", "Giants"]
   sports_array << "Jets"
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(sports_array, element)
    sports_array =["Knicks", "Yankess","Nets", "Giants"]
    sports_array.unshift ("Mets")
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array(sports_array)
    sports_array =["Knicks", "Yankess","Nets", "Giants"]
    sports_array.pop
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array(sports_array)
    sports_array =["Knicks", "Yankess","Nets", "Giants"]
    sports_array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(sports_array, index_number)
    sports_array =["Knicks", "Yankess","Nets", "Giants"]
    sports_array.index("Nets")
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(sports_array)
+   sports_array =["Knicks", "Yankess","Nets", "Giants"]
+   sports_array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(sports_array)
+   sports_array =["Knicks", "Yankess","Nets", "Giants"]
+   sports_array[-1]
 end
